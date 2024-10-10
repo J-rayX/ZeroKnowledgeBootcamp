f10033b29ab063f8bdc1528bd80edb93d1b37823
jkaylight <jkaylight@gmail.com>
2024-10-10 22:38:40 +0100

    Homework4 completed🎉🎉

--- rust/homeworks/homework4/functions/functions3.rs
+++ rust/homeworks/homework4/functions/functions3.rs
@@ -1,13 +1,11 @@
 // functions3.rs
 // Make me compile! Execute `zustlings hint functions3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    call_this();
+    call_this(4);
 }
 
-fn call_this(num: u32) {
+fn call_this(num:u32) {
     for i in 0..num {
         println!("Loop now {}", i + 1);
     }
